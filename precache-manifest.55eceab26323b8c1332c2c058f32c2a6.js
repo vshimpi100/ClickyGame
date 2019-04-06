44cfac8ba2a524fbbb23534cf36693a8d65b7c89
Vivek Shimpi <vshimpi100@gmail.com>
2019-04-06 16:23:50 -0400

    Updates

---
+++ precache-manifest.55eceab26323b8c1332c2c058f32c2a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "99bcdc78f7892a568062",
+    "url": "/ClickyGame/static/js/main.fd5d305c.chunk.js"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/ClickyGame/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "99bcdc78f7892a568062",
+    "url": "/ClickyGame/static/css/main.ad40417d.chunk.css"
+  },
+  {
+    "revision": "a370d8a13b40c105791cbceffac1487c",
+    "url": "/ClickyGame/index.html"
+  }
+];
